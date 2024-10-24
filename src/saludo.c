53d07fb4edaca2b15322000a3df96a4c1072a19f
Antonio Monge <amongem@iesrafaelalberti.es>
2024-10-24 21:47:11 +0200

    respectivos programas

---
+++ src/saludo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hola. Este programa está hecho en el lenguaje de programación: C\n");
+    return 0;
+}
+
---
+++ src/saludo.py
@@ -0,0 +1,1 @@
+print("Hola. Este programa está hecho en el lenguaje de programación: Python")
